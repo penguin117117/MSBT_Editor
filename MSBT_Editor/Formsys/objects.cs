56942bac9d10108c0f55a9858c65e50ad402b998
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2021-08-13 10:00:15 -0400

    Reworked EN Icon List

--- MSBT_Editor/Formsys/objects.cs
+++ MSBT_Editor/Formsys/objects.cs
@@ -17,13 +17,40 @@ namespace MSBT_Editor.Formsys
         protected static TextBox txtb24 = Form1.Form1Instance.textBox24;
         protected static TextBox txtb27 = Form1.Form1Instance.textBox27;
 
-        //button
+        //button 1~9
         protected static Button listadd = Form1.Form1Instance.AddListText;
         protected static Button listdelete = Form1.Form1Instance.DeleteListText;
+        protected static Button button1 = Form1.Form1Instance.button1;
+        protected static Button button2 = Form1.Form1Instance.button2;
+        protected static Button button3 = Form1.Form1Instance.button3;
+        protected static Button button4 = Form1.Form1Instance.button4;
+        protected static Button button5 = Form1.Form1Instance.button5;
+        protected static Button button6 = Form1.Form1Instance.button6;
+        protected static Button button7 = Form1.Form1Instance.button7;
+        protected static Button button8 = Form1.Form1Instance.button8;
+        protected static Button button9 = Form1.Form1Instance.button9;
+
+        //Button 10~19
+        protected static Button button10 = Form1.Form1Instance.button10;
+        protected static Button button11 = Form1.Form1Instance.button11;
+        protected static Button button12 = Form1.Form1Instance.button12;
+        protected static Button button13 = Form1.Form1Instance.button13;
+        protected static Button button14 = Form1.Form1Instance.button14;
+        protected static Button button15 = Form1.Form1Instance.button15;
+        protected static Button button16 = Form1.Form1Instance.button16;
+        protected static Button button17 = Form1.Form1Instance.button17;
+        protected static Button button18 = Form1.Form1Instance.button18;
+        protected static Button button19 = Form1.Form1Instance.button19;
+
         protected static Button button21 = Form1.Form1Instance.button21;
         protected static Button button22 = Form1.Form1Instance.button22;
         protected static Button button23 = Form1.Form1Instance.button23;
         protected static Button button24 = Form1.Form1Instance.button24;
+
+        protected static Button button26 = Form1.Form1Instance.button26;
+        protected static Button button27 = Form1.Form1Instance.button27;
+        protected static Button button28 = Form1.Form1Instance.button28;
+        protected static Button button29 = Form1.Form1Instance.button29;
 
         //label 0～9
         protected static Label labeltxt01 = Form1.Form1Instance.label1;
@@ -43,7 +70,15 @@ namespace MSBT_Editor.Formsys
         protected static Label labeltxt13 = Form1.Form1Instance.label13;
         protected static Label labeltxt15 = Form1.Form1Instance.label15;
 
+        protected static Label labeltxt17 = Form1.Form1Instance.label17;
+        protected static Label labeltxt18 = Form1.Form1Instance.label18;
+        protected static Label labeltxt19 = Form1.Form1Instance.label19;
+
+
         //label 20～29
+        protected static Label labeltxt20 = Form1.Form1Instance.label20;
+        protected static Label labeltxt21 = Form1.Form1Instance.label21;
+        protected static Label labeltxt22 = Form1.Form1Instance.label22;
         protected static Label labeltxt23 = Form1.Form1Instance.label23;
         protected static Label labeltxt24 = Form1.Form1Instance.label24;
         protected static Label labeltxt25 = Form1.Form1Instance.label25;
@@ -56,6 +91,7 @@ namespace MSBT_Editor.Formsys
         protected static Label labeltxt30 = Form1.Form1Instance.label30;
         protected static Label labeltxt31 = Form1.Form1Instance.label31;
         protected static Label labeltxt32 = Form1.Form1Instance.label32;
+        protected static Label labeltxt33 = Form1.Form1Instance.label33;
         protected static Label labeltxt34 = Form1.Form1Instance.label34;
         protected static Label labeltxt35 = Form1.Form1Instance.label35;
         protected static Label labeltxt39 = Form1.Form1Instance.label39;
@@ -66,7 +102,26 @@ namespace MSBT_Editor.Formsys
         protected static ListBox list2 = Form1.Form1Instance.listBox2;
         protected static ListBox list3 = Form1.Form1Instance.listBox3;
 
+        //
+        protected static TreeView treeview1 = Form1.Form1Instance.treeView1;
+
+        //
+        protected static CheckBox chb1 = Form1.Form1Instance.checkBox1;
+
+        //combbox
+        protected static ComboBox combo1 = Form1.Form1Instance.comboBox1;
+        protected static ComboBox combo2 = Form1.Form1Instance.comboBox2;
+        protected static ComboBox combo3 = Form1.Form1Instance.comboBox3;
+        protected static ComboBox combo4 = Form1.Form1Instance.comboBox4;
+        protected static ComboBox combo5 = Form1.Form1Instance.comboBox5;
+        protected static ComboBox combo6 = Form1.Form1Instance.comboBox6;
+        protected static ComboBox combo7 = Form1.Form1Instance.comboBox7;
+
         //groupbox
+        protected static GroupBox groupbox3 = Form1.Form1Instance.groupBox3;
+        protected static GroupBox groupbox4 = Form1.Form1Instance.groupBox4;
+        protected static GroupBox groupbox5 = Form1.Form1Instance.groupBox5;
+        protected static GroupBox groupbox6 = Form1.Form1Instance.groupBox6;
         protected static GroupBox groupbox8 = Form1.Form1Instance.groupBox8;
 
         //tab
@@ -75,6 +130,11 @@ namespace MSBT_Editor.Formsys
         protected static TabPage tabp2 = Form1.Form1Instance.tabPage2;
         protected static TabPage tabp3 = Form1.Form1Instance.tabPage3;
         protected static TabPage tabp4 = Form1.Form1Instance.tabPage4;
+
+        protected static TabPage tabp6 = Form1.Form1Instance.tabPage6;
+        protected static TabPage tabp7 = Form1.Form1Instance.tabPage7;
+        protected static TabPage tabp8 = Form1.Form1Instance.tabPage8;
+        protected static TabPage tabp9 = Form1.Form1Instance.tabPage9;
 
         //menu
         protected static ToolStripMenuItem tlmi_file = Form1.Form1Instance.ファイルToolStripMenuItem;
@@ -89,47 +149,11 @@ namespace MSBT_Editor.Formsys
         protected static ToolStripStatusLabel tssl1 = Form1.Form1Instance.toolStripStatusLabel1;
         protected static ToolStripStatusLabel tssl2 = Form1.Form1Instance.toolStripStatusLabel2;
         protected static ToolStripStatusLabel tssl4 = Form1.Form1Instance.toolStripStatusLabel4;
+        protected static ToolStripStatusLabel tssl6 = Form1.Form1Instance.toolStripStatusLabel6;
+        protected static ToolStripStatusLabel tssl7 = Form1.Form1Instance.toolStripStatusLabel7;
 
-        //EN Translation
-
-        protected static TabPage tabp6 = Form1.Form1Instance.tabPage6;
-        protected static Button button1 = Form1.Form1Instance.button1;
-        protected static Button button2 = Form1.Form1Instance.button2;
-        protected static Button button3 = Form1.Form1Instance.button3;
-        protected static Button button4 = Form1.Form1Instance.button4;
-
-        protected static Button button7= Form1.Form1Instance.button7;
-        protected static Button button8= Form1.Form1Instance.button8;
-        protected static Button button9= Form1.Form1Instance.button9;
-        protected static Button button10 = Form1.Form1Instance.button10;
-        protected static Button button11 = Form1.Form1Instance.button11;
-        protected static Button button12 = Form1.Form1Instance.button12;
-        protected static Button button13 = Form1.Form1Instance.button13;
-        protected static Button button17 = Form1.Form1Instance.button17;
-        protected static Button button18 = Form1.Form1Instance.button18;
-        protected static Button button19 = Form1.Form1Instance.button19;
-        protected static Button button14 = Form1.Form1Instance.button14;
-        protected static Button button15 = Form1.Form1Instance.button15;
-        protected static Button button16 = Form1.Form1Instance.button16;
-
-        protected static ComboBox box1text = Form1.Form1Instance.comboBox1;
-        protected static ComboBox box2text = Form1.Form1Instance.comboBox2;
-        protected static ComboBox box3text = Form1.Form1Instance.comboBox3;
-        protected static ComboBox box4text = Form1.Form1Instance.comboBox4;
-
-        protected static ComboBox IconBox1 = Form1.Form1Instance.comboBox5;
-        protected static ComboBox IconBox2 = Form1.Form1Instance.comboBox6;
-        protected static ComboBox IconBox3 = Form1.Form1Instance.comboBox7;
-
-        protected static TabPage tabpage7 = Form1.Form1Instance.tabPage7;
-        protected static TabPage tabpage8 = Form1.Form1Instance.tabPage8;
-        protected static TabPage tabpage9 = Form1.Form1Instance.tabPage9;
-
-        protected static Label label20 = Form1.Form1Instance.label20;
-        protected static Label label21 = Form1.Form1Instance.label21;
-        protected static Label label22 = Form1.Form1Instance.label22;
-
-        protected static GroupBox box3 = Form1.Form1Instance.groupBox3;
-
+        //
+        protected static TextBox unknowntag = Form1.Form1Instance.UnknownTag;
+       
     }
 }
